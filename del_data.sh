c4f2ae26d7ceadd14d75ed2fad4ef203250b997b
Peter Markewitz <peter@markewitz.org>
2024-11-11 06:34:30 +0000

    add del_data.sh

---
+++ del_data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -f ./dataStore/history/cases/districts.feather
+rm -f ./dataStore/history/cases/states.feather
+rm -f ./dataStore/history/deaths/districts.feather
+rm -f ./dataStore/history/deaths/states.feather
+rm -f ./dataStore/history/incidence/districts.feather
+rm -f ./dataStore/history/incidence/states.feather
+rm -f ./dataStore/history/recovered/districts.feather
+rm -f ./dataStore/history/recovered/states.feather
+rm -f ./dataStore/historychanges/cases/districts_Diff.csv
+rm -f ./dataStore/historychanges/cases/states_Diff.csv
+rm -f ./dataStore/historychanges/deaths/districts_Diff.csv
+rm -f ./dataStore/historychanges/deaths/states_Diff.csv
+rm -f ./dataStore/historychanges/incidence/districts_Diff.csv
+rm -f ./dataStore/historychanges/incidence/states_Diff.csv
+rm -f ./dataStore/historychanges/recovered/districts_Diff.csv
+rm -f ./dataStore/historychanges/recovered/states_Diff.csv
+rm -f ./dataStore/meta/meta.json
